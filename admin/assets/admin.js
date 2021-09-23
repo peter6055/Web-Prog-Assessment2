dc2cd9dec5b53df9f2d8160d6ada21e17381149c
SHIHMING LIU s3789585 <s3789585@student.rmit.edu.au>
2021-09-24 00:48:18 +1000

    a2 a,b,c,d,e,f,h,i final part g half (API finish) part j half (two chart left)

--- admin/assets/admin.js
+++ admin/assets/admin.js
@@ -1,36 +1,346 @@
-function loadServiceType() {
-    $(document).ready(function () {
+// function nameSelectValidation() {
+//     // source: https://stackoverflow.com/questions/2901125/jquery-validate-required-select
+//     $.validator.addMethod("valueNotEquals", function (value, element, arg) {
+//         return arg !== value;
+//     }, "Value must not equal arg.");
+//     // source: https://stackoverflow.com/questions/2901125/jquery-validate-required-select
+//
+//
+//     $(document).ready(function () {
+//
+//         // user select bar jquery validation
+//         $("#user-bar").validate({
+//             rules: {
+//                 user: {
+//                     valueNotEquals: "none"
+//                 }
+//             },
+//             messages: {
+//                 user: {
+//                     valueNotEquals: "You need to select a user!"
+//                 }
+//             },
+//             errorElement: "div",
+//             errorClass: 'errorMsg text-danger pt-2 ps-2',
+//         });
+//     });
+// }
 
-        // start ajax inquiries
+
+function searchServiceQuery() {
+    var search = $('#search').val().trim();
+    $.ajax({
+        url: 'includes/api/searchServiceQuery.php',
+        data: {
+            search: search,
+        },
+
+        // ---------------- if successful insert to the page -------------------
+        success: function (response) {
+            var result = JSON.parse(response);
+            var responseCode = result[0];
+            var responseMsg = result[1];
+
+            $("#errorMsgSearch").empty();
+            $("#service").empty();
+
+            if (responseCode == 0) {
+                $('#errorMsgSearch').append('<p class="">' + responseMsg + '</p></br>');
+                // no search then recover to the original look (4 services)
+                for(let count = 0 ; count < 4 ; count++){
+
+                    if(count == 0){
+                        printServiceBtn("yoga");
+                    } else if(count == 1){
+                        printServiceBtn("meditation");
+                    } else if(count == 2){
+                        printServiceBtn("stretching");
+                    } else if(count == 3){
+                        printServiceBtn("healthy-habits");
+                    }
+                }
+
+                // will need to empty stat when user clear search fields
+                $("#chart").remove();
+
+            } else if (responseCode == 1){
+                printServiceBtn(responseMsg);
+                // out side container
+                for(let count = 0 ; count < 3 ; count++){
+                    $('#service').append('<div class="col flex-column d-flex"></div>');
+                }
+            }
+        },
+        error: function (request, error) {
+            alert("System Error! Please try again");
+        }
+    });
+}
+
+// function use to print service button (radio)
+function printServiceBtn(responseMsg){
+    // out side container
+    $('#service').append('<div class="col flex-column d-flex" id="' +responseMsg+ '_col"></div>');
+    // radio
+    $('#' +responseMsg+ '_col').append('<input type="radio" class="btn-check" name="service" id="'+responseMsg+'" value="'+responseMsg+'" autocomplete="off">');
+    // label
+    $('#' +responseMsg+ '_col').append('<label class="border h-100 w-100 rounded bg-white shadow-sm py-4 btn btn-secondary" id="'+responseMsg+'_btn" for="'+responseMsg+'">  <img class="mt-4" src="assets/images/'+responseMsg+'.svg?>" height="210px">  </label>');
+    // add click change color to label
+    $('#' +responseMsg+ '_btn').attr("onclick", "checkedColor('" +responseMsg+ "');");
+    // service name content
+    $('#' +responseMsg+ '_col').append('<p class="lead text-center mt-3" id="' +responseMsg+ '_font">' +responseMsg+ '</p>');
+}
+
+
+
+function nameSelected() {
+
+    // clean every thing after re-select user.
+    $("#errorMsgSearch").empty();
+    $("#service").empty();
+    $("#chart").empty();
+    $("#name-select-error-msg").remove();
+
+    if($("#user option:selected").val() == "none"){
+        $('#user-bar').append('<p class="text-danger pt-2 ps-2" id="name-select-error-msg"> You need to select a user! </p>');
+        $('#after-selected-name').attr("style", "display:none");
+        return 0;
+    }
+
+    // rewrite the service selection bar first.
+    for(let count = 0 ; count < 4 ; count++){
+        if(count == 0){
+            printServiceBtn("yoga");
+        } else if(count == 1){
+            printServiceBtn("meditation");
+        } else if(count == 2){
+            printServiceBtn("stretching");
+        } else if(count == 3){
+            printServiceBtn("healthy-habits");
+        }
+    }
+    $('#after-selected-name').attr("style", "display:block");
+}
+
+
+
+
+
+
+function checkedColor(actionName) {
+
+    $('#yoga_btn').attr("style", "border-color: #dee2e6 !important;");
+    $('#yoga_font').attr("style", "color: #000000 !important;");
+    $('#meditation_btn').attr("style", "border-color: #dee2e6 !important;");
+    $('#meditation_font').attr("style", "color: #000000 !important;");
+    $('#stretching_btn').attr("style", "border-color: #dee2e6 !important;");
+    $('#stretching_font').attr("style", "color: #000000 !important;");
+    $('#healthy-habits_btn').attr("style", "border-color: #dee2e6 !important;");
+    $('#healthy-habits_font').attr("style", "color: #000000 !important;");
+    $('#chart').remove();
+
+
+    if (actionName == "yoga") {
+        $('#yoga_btn').attr("style", "border-color: #3463cc !important;");
+        $('#yoga_font').attr("style", "color: #3463cc !important;");
+    } else if (actionName == "meditation") {
+        $('#meditation_btn').attr("style", "border-color: #cc5b4b !important;");
+        $('#meditation_font').attr("style", "color: #cc5b4b !important;");
+    } else if (actionName == "stretching") {
+        $('#stretching_btn').attr("style", "border-color: #8c4d97 !important;");
+        $('#stretching_font').attr("style", "color: #8c4d97 !important;");
+    } else if (actionName == "healthy-habits") {
+        $('#healthy-habits_btn').attr("style", "border-color: #b38b1e !important;");
+        $('#healthy-habits_font').attr("style", "color: #b38b1e !important;");
+    }
+
+
+
+
+    // draw chart
+
+    
+    
+    // get user's selection and input first
+    // var user_email = $('#user').val().trim();
+    var user_email = $('[name="user"]').val();
+
+    // empty the diagram container
+    $('#after-selected-name').append('<div class="row border p-4 m-1" id="chart"></div>');
+
+    // check which diagram should print
+    if (actionName == "yoga" || actionName == "meditation" || actionName == "stretching") {
+        // first title and diagram
+        $('#chart').append('<h3 class="pt-5">What type of service user has enrolled?</h3>');
+        $('#chart').append('<div class="pt-4" id="service_type_bar_chart_container"> <canvas id="service_type_bar_chart"></canvas> </div>');
+
+        // second title and diagram
+        $('#chart').append('<h3 class="pt-5">How long did the user participate in this service? (Mins) </h3>');
+        $('#chart').append('<div class="pt-4" id="service_duration_bar_chart_container"> <canvas id="service_duration_bar_chart"></canvas> </div>');
+
+        // third title and diagram
+        $('#chart').append('<h3 class="pt-5">When did the user use the services?</h3>');
+        $('#chart').append('<div class="pt-4" id="service_time_line_graph_container"> <canvas id="service_time_line_graph"></canvas> </div>');
+        
+        
+        // ---------------------------- global chart setting ----------------------------
+        // background color
+        const backgroundColor = [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+        ];
+
+        // border color
+        const borderColor = [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+        ];
+
+
+
+        // ---------------------------- first diagram config (service_type_bar_chart) ----------------------------
         $.ajax({
-            url: 'https://reg.bom.gov.au/fwo/IDV60901/IDV60901.95936.json',
-            crossDomain: true,
-            dataType: 'jsonp',
-            type : "GET",
+            url: 'includes/api/getChartData.php',
+            data: {
+                user_email: user_email,
+                service_name: actionName,
+                chart_name: "service_type_bar_chart",
+            },
+
             success: function (response) {
+                var result = JSON.parse(response);
 
-                var results = JSON.parse(response);
+                // data y
+                const service_type_bar_chartData = {
+                    labels: result[1],
+                    datasets: [{
+                        label: 'What type of ' +actionName+ ' user has enrolled?',
+                        backgroundColor: backgroundColor,
+                        borderColor: borderColor,
+                        borderWidth: 1 ,
+                        data: result[0],
+                    }]
+                };
 
-                alert(results['observations']['data'][0]['air_temp']);
+                const service_type_bar_chartConfig = {
+                    type: 'bar',
+                    data: service_type_bar_chartData,
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    },
+                };
 
+                var service_type_bar_chart = new Chart(
+                    document.getElementById('service_type_bar_chart'),
+                    service_type_bar_chartConfig
+                );
+
+            },
+            error: function (request, error) {
+                alert("System Error! Please try again");
             }
         });
 
-        // alert("start");
-        //
-        // var myHeaders = new Headers();
-        // myHeaders.append("Cookie", "BIGipServer~Production~PROD_REG_TCP80_HTTP_POOL=rd1o00000000000000000000ffff86b2fd85o80");
-        //
-        // var requestOptions = {
-        //     method: 'GET',
-        //     headers: myHeaders,
-        //     redirect: 'follow'
-        //
-        // };
-        //
-        // fetch("https://reg.bom.gov.au/fwo/IDV60901/IDV60901.95936.json", requestOptions)
-        //     .then(response => response.text())
-        //     .then(result => console.log(result))
-        //     .catch(error => console.log('error', error));
-    });
+
+
+        // ---------------------------- second diagram config (service_type_bar_chartData) ----------------------------
+        $.ajax({
+            url: 'includes/api/getChartData.php',
+            data: {
+                user_email: user_email,
+                service_name: actionName,
+                chart_name: "service_duration_bar_chart",
+            },
+
+            success: function (response) {
+                var result = JSON.parse(response);
+
+                // data y
+                const service_type_bar_chartData = {
+                    labels: result[1],
+                    datasets: [{
+                        label: 'How long did the user participate in ' +actionName+ '? (Mins)',
+                        backgroundColor: backgroundColor,
+                        borderColor: borderColor,
+                        borderWidth: 1 ,
+                        data: result[0],
+                    }]
+                };
+
+                const service_duration_bar_chartConfig = {
+                    type: 'bar',
+                    data: service_type_bar_chartData,
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    },
+                };
+
+                var service_type_bar_chart = new Chart(
+                    document.getElementById('service_duration_bar_chart'),
+                    service_duration_bar_chartConfig
+                );
+
+            },
+            error: function (request, error) {
+                alert("System Error! Please try again");
+            }
+        });
+
+
+    //     // ---------------------------- third diagram config (service_time_line_graph) ----------------------------
+        const service_time_line_graphConfig = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June',],
+            type: 'line',
+            data: data,
+            options: {
+                responsive : true
+            }
+        };
+
+        var service_type_bar_chart = new Chart(
+            document.getElementById('service_time_line_graph'),
+            service_time_line_graphConfig
+        );
+
+
+    // } else if (actionName == "healthy-habits") {
+    //     // first title and diagram
+    //     $('#chart').append('<h3 class="pt-5">How many calories serving in a day?</h3>');
+    //     $('#chart').append('<div class="pt-4" id="serving_per_diet_pie_chart_container"> <canvas id="serving_per_diet_pie_chart"></canvas> </div>');
+    //
+    //     // ---------------------------- first diagram config ----------------------------
+    //     const serving_per_diet_pie_chartConfig = {
+    //         type: 'pie',
+    //         data: data,
+    //         options: {
+    //             responsive : true
+    //         }
+    //     };
+    //
+    //
+    //     var service_type_bar_chart = new Chart(
+    //         document.getElementById('serving_per_diet_pie_chart'),
+    //         serving_per_diet_pie_chartConfig
+    //     );
+
+    }
+
 }
--- admin/portal.php
+++ admin/portal.php
@@ -25,7 +25,6 @@ error_reporting(E_ALL);
     <?php require_once('includes/functions.php'); ?>
 
 
-
     <!-- Google font API-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -47,12 +46,19 @@ error_reporting(E_ALL);
     <!--jQuery Data Validation-->
     <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
 
+    <!-- Chart.js-->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <!--js-->
+    <script type="text/javascript" src="assets/admin.js"></script>
+
+    <!--css-->
     <script type="text/javascript" src="assets/admin.js"></script>
 
 </head>
 
-<body class="d-flex flex-column" onload="loadServiceType()">
+<body class="d-flex flex-column" onload="nameSelectValidation();">
+<!-- Header -->
 <header class="p-3 bg-dark text-white">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -64,20 +70,57 @@ error_reporting(E_ALL);
                 <li><a class="nav-link px-2 text-secondary">Admin Portal</a></li>
             </ul>
 
+            <!-- Get weather info -->
             <?php require_once "includes/api/getWeather.php" ?>
 
-            <form method="post" class="text-end" >
+            <form method="post" class="text-end">
                 <button type="submit" class="btn btn-warning" name="back" value="back">Back to Client Site</button>
             </form>
         </div>
     </div>
 </header>
 
-
+<!-- Body content -->
 <div class="d-flex justify-content-start align-items-stretch">
+    <div class="container vh-100 mt-3"></br>
 
-    <div class="container vh-100"></br>
+        <!-- User selection -->
+        <form class="form-inline" id="user-bar">
+            <label for="user-dropdown" class="pb-1">Please Select a User</label>
+            <select class="form-control" id="user" name="user" onchange="nameSelected()">
+                <option selected value="none">Please select....</option>
+                <?php require_once "includes/api/getUser.php" ?>
+            </select>
+        </form>
+        </br>
 
+
+        <!-- This area will only display when user name is selected -->
+        <div id="after-selected-name" style="display: none;">
+            <div class="row border p-4 m-1">
+
+                <!-- Form to search service  -->
+                <form class="form-inline d-flex" id="search-bar" onsubmit="return false">
+                    <input class="form-control mr-sm-2 me-2" type="input" id="search" name="search"
+                           placeholder="Search for service name..." aria-label="Search" onkeyup="searchServiceQuery()">
+                    <button class="btn btn-primary my-2 my-sm-0" type="submit" onclick="searchServiceQuery()">Search
+                    </button>
+                </form>
+                </br></br>
+                <div class=" text-danger pt-2 ps-3" id="errorMsgSearch"></div>
+                </br>
+
+
+                <!-- Form to select service  -->
+                <form name="my-service-selection" id="my-service-selection" onsubmit="return false">
+                    <!--Service List -->
+                    <div class="row align-items-start align-items-stretch" id="service">
+                    </div>
+                    </br></br>
+                </form>
+
+            </div>
+        </div> </br></br>
     </div>
 </div>
 </body>

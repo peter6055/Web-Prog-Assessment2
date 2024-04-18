36fcabfd3c2390b20d0b2a402ce347156c1d1d9f
Peter Liu <59694246+peter6055@users.noreply.github.com>
2024-04-19 09:30:57 +1000

    Update api-header.php

--- admin/includes/api/api-header.php
+++ admin/includes/api/api-header.php
@@ -4,7 +4,7 @@
 //error_reporting(E_ALL);
 
 // Make DB connection.
-$mysqli = new mysqli("www.databaseaustralia.com", "s3789585_a2", "Peter0466555592_phpmyadmin", "s3789585_a2");
+$mysqli = new mysqli("", "", "", "");
 
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
